8413fc5e6663a34db80da4c7957d3cc9ddb08e0f
Luper Rouch <luper.rouch@gmail.com>
2014-10-27 16:41:22 +0100

    broke japper.monitoring.plugins in 2 modules, to separate models from the rest (since they need django settings to be loaded before import)

---
+++ japper/monitoring/plugins/models.py
@@ -0,0 +1,56 @@
+import abc
+
+import six
+from django.db import models
+from django.db.models.base import ModelBase
+
+
+class MonitoringSourceBase(ModelBase, abc.ABCMeta):
+
+    pass
+
+
+class MonitoringSource(six.with_metaclass(MonitoringSourceBase, models.Model)):
+    '''
+    Base model for monitoring sources.
+    '''
+
+    name = models.CharField(max_length=255, unique=True)
+    active = models.BooleanField(default=True)
+
+    @abc.abstractmethod
+    def get_check_results(self):
+        '''
+        Return a list of check results.
+
+        This must return a list of dicts of the form::
+
+            {
+                'host_name': 'foo.com',
+                'check_name': 'memory',
+                'status': CheckStatus.passing,
+                'metrics': {
+                    'free': '2G',
+                    'used': '6G',
+                    'swap': 0,
+                },
+            }
+
+        Metrics must be a dict containing integers, floats or human-readable
+        byte sizes values. It can also be None if the check does not have
+        associated metrics.
+        '''
+
+    def get_removed_hosts(self):
+        '''
+        Return the list of hosts names that must be removed from monitoring.
+
+        The default implementation returns an empty list.
+        '''
+        return []
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
