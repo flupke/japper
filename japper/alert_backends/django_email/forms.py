1601c3600ce3cfa785dc42ab4d2a2be0a7f949df
Luper Rouch <luper.rouch@gmail.com>
2014-11-05 00:35:14 +0100

    cleanup

--- japper/alert_backends/django_email/forms.py
+++ japper/alert_backends/django_email/forms.py
@@ -1,16 +1,9 @@
 from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 
 from .models import AlertSink
 
 
 class AlertSinkForm(forms.ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super(AlertSinkForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', 'Submit'))
 
     class Meta:
         model = AlertSink
