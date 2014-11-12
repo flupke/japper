35ca10ef0a16a3e6108e2ad34e3ba3a15d6da88f
Luper Rouch <luper.rouch@gmail.com>
2014-11-12 19:06:57 +0100

    added frontpage url

--- japper/urls.py
+++ japper/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
-from japper.monitoring.plugins import get_url_patterns
+from japper.monitoring.views import StatesList
 
 
-urlpatterns = get_url_patterns('',
-    # Examples:
-    # url(r'^$', 'japper.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns('',
+    url(r'^$',login_required(StatesList.as_view(problems_only=True)),
+        name='frontpage'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^monitoring/', include('japper.monitoring.urls')),
     url(r'^login/$', auth_views.login, name='login'),
