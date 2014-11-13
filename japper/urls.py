5f0fbeaf69e52bdac5b3a49a8876021640e49fdd
Luper Rouch <luper.rouch@gmail.com>
2014-11-13 10:51:41 +0100

    frontpage redirects to problems page instead of aliasing it

--- japper/urls.py
+++ japper/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.decorators import login_required
+from django.views.generic import RedirectView
 
-from japper.monitoring.views import StatesList
 from japper.monitoring.plugins import get_url_patterns
 
 
 urlpatterns = get_url_patterns('',
-    url(r'^$',login_required(StatesList.as_view(problems_only=True)),
+    url(r'^$', RedirectView.as_view(pattern_name='monitoring_problems'),
         name='frontpage'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^monitoring/', include('japper.monitoring.urls')),
